86e692bc3f3abd4c87ad8b2dee158bdff4356236
José Matheus <132094255+jose-matheus@users.noreply.github.com>
2024-11-14 18:17:44 -0300

    commit test

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    nome: str
+    valor: float
+    is_oferta: Union[bool, None] = None
+
+@app.get("/")
+def read_root():
+    return {"msg": "Hello World"}
+
+@app.get("/itens/{id}")
+def le_id_item(id: int):
+    return id
+
+@app.get("/itens/")
+def le_item(item: Item):
+    return item
